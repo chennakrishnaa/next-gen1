7be505783c40d6cdd09c56150502700a3893d071
Venkatarao Gali <g.venkat@yahoo.com>
2013-06-30 23:59:55 +0200

    added sass files and modified grunt to use sass watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,17 +3,59 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg:grunt.file.readJSON("package.json"),
 		uglify:{
-			options: {
-				banner: '/*!<%= pkg.name %> <%= grunt.template.today("mm-dd-yyyy") %>*/\n'
-			}
+			
 			build: {
-				src:'<%= pkg.src %>/js/*.js'
-				dest:'<%= pkg.des %>/js/*.min.js'
+				src:'dest/js/<%= pkg.name %>.js',
+				dest:'dest/<%= pkg.name%>.min.js'
+				
 			}
+		},
+		concat:{
+			options: {
+				seperator: ';',
+				banner: '/*!<%= pkg.name %> <%= grunt.template.today("mm-dd-yyyy") %>*/\n'
+			},
+			build: {
+				src:['js/*.js'],
+				dest:'dest/js/<%= pkg.name %>.js'
+				/*files: [{
+					expand: true,
+					cwd:'js/',
+					src:'*.js',
+					dest:'dest/js/',
+					ext:'.min.js'
+				}]*/
+			}
+		},
+		copy: {
+			main: {
+				files: [
+				//{expand:true, src:['**','!package.json','!Gruntfile.js','!node_modules/**'], dest:'build/'}
+				{expand:true, src:['css/scss/**'], dest:'build/'},
+				{expand:true, src:['css/next-gen1.css'], dest:'build/'}
+				]
+			}
+		},
+		sass: {
+			dist: {
+					src: ['css/scss/next-gen1.scss'],
+					dest:'css/next-gen1.css' 
+			}
+		},
+		watch: {
+			files: ['css/scss/**'],
+			tasks:['sass','copy']
 		}
 
 	});
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['uglify']);
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['sass'])
+	//grunt.registerTask('watch',['watch']);
+	//grunt.registerTask('default', ['sass','copy']);
+	//grunt.registerTask('default', ['concat', 'uglify']);
 
 };
